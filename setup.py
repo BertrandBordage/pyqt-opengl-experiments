fab4743b255b76050eb935e4bb3c47d013caccde
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-08-07 12:35:40 +0200

    Fait du moteur une extension Cython.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+
+ext_modules = [Extension('engine', ['engine.pyx'])]
+
+
+setup(
+    name='PyQt OpenGL experiments',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules
+)
