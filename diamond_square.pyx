8e6d516ab0fb941ab91e0ce80b984402138f52b9
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-08-12 00:17:31 +0200

    Fait de la génération de tuiles avec le diamond-square.

--- diamond_square.pyx
+++ diamond_square.pyx
@@ -28,8 +28,8 @@ cdef enum SIDE:
     SQUARE = 2
 
 
-cdef inline double nearby_sum(double[:, :] m, int x, int y,
-                              int step, int size, SIDE side) nogil:
+cdef inline double _nearby_sum(double[:, :] m, int x, int y,
+                               int step, int size, SIDE side) nogil:
     cdef int xm, xM, ym, yM
     xm = _get_real_min(x - step, size)
     xM = _get_real_max(x + step, size)
@@ -41,34 +41,55 @@ cdef inline double nearby_sum(double[:, :] m, int x, int y,
     return m[x, ym] + m[xm, y] + m[xM, y] + m[x, yM]
 
 
+cdef inline void _update_borders(double[:, :] m_view, int x, int y) nogil:
+    if x == 0:
+        m_view[-1, y] = m_view[0, y]
+    elif y == 0:
+        m_view[x, -1] = m_view[x, 0]
+
+
+cdef inline float _random_coef(unsigned int step, float smoothing) nogil:
+    return pow(<float>step, smoothing)
+
+
 cpdef np.ndarray[double, ndim=2] continuous_map(
         int size, int amplitude=15, float smoothing=0.8, bint save=True):
     cdef unsigned int orig_size, step, two_steps, x, y
-    cdef float random_coef
     orig_size = size
     size += (size + 1) % 2
 
     cdef np.ndarray[double, ndim=2] m = np.zeros((size, size))
     cdef double[:, :] m_view = m
 
+
     step = size // 2
+    cdef float random_coef = _random_coef(step, smoothing)
+    m_view[0, 0] = m_view[-1, 0] = m_view[0, -1] = m_view[-1, -1] = random_coef
+
     while True:
-        random_coef = pow(<float>step, <float>smoothing)
+        random_coef = _random_coef(step, smoothing)
         two_steps = step * 2
 
         # Diamond
         for x from step <= x < size by two_steps:
             for y from step <= y < size by two_steps:
-                m_view[x, y] = (nearby_sum(m_view, x, y, step, size, DIAMOND)
+                if m_view[x, y]:
+                    continue
+
+                m_view[x, y] = (_nearby_sum(m_view, x, y, step, size, DIAMOND)
                                 + uniform(-random_coef, random_coef)) / 4
+
+                _update_borders(m_view, x, y)
 
         # Square
         for x from 0 <= x < size by step:
             for y from 0 <= y < size by step:
-                if m[x, y]:
+                if m_view[x, y]:
                     continue
 
-                m_view[x, y] = nearby_sum(m_view, x, y, step, size, SQUARE) / 4
+                m_view[x, y] = _nearby_sum(m_view, x, y, step, size, SQUARE) / 4
+
+                _update_borders(m_view, x, y)
 
         if step == 1:
             break
