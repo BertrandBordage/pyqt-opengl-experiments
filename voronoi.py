f998219dca64db32501a600d470080cf6c914c01
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-08-11 18:01:43 +0200

    Permet d'écrire les coefficients de Voronoi.

--- voronoi.py
+++ voronoi.py
@@ -4,26 +4,27 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 from utils import save_to_img
 
 
-def distance_to_array(array, value):
-    d = np.abs(array - value)
-    distances = np.sqrt(d[:, 0] ** 2 + d[:, 1] ** 2)
-    return distances.min()
+def distances_from_array(array, value):
+    d = array - value
+    return np.sqrt(d[:, 0] ** 2 + d[:, 1] ** 2)
 
 
-def voronoi_matrix(size, n_points=30, save=True, plot=False):
+def voronoi_matrix(size, n_points=15, coefs=(-1, 1), save=True, plot=False):
     points = np.random.randint(0, size, (n_points, 2))
-
-    v = Voronoi(points)
+    voronoi = Voronoi(points)
 
     m = np.zeros((size, size))
     for x in range(m.shape[0]):
         for y in range(m.shape[1]):
-            m[x, y] = -distance_to_array(v.vertices, (x, y))
+            distances = distances_from_array(voronoi.vertices, (x, y))
+            distances.sort()
+            m[x, y] = sum(coef * distances[i]
+                          for i, coef in enumerate(coefs) if coef)
 
     if save:
         save_to_img(m.copy())
     if plot:
-        voronoi_plot_2d(v)
+        voronoi_plot_2d(voronoi)
         pyplot.show()
 
     return m
