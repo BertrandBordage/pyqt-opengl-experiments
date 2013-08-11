0c5aae989daf0ae54c24fd28317c9277b552ab60
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-08-11 17:42:43 +0200

    Rend 10 fois plus rapide l'algorithme de Voronoi.

--- voronoi.py
+++ voronoi.py
@@ -4,19 +4,13 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 from utils import save_to_img
 
 
-def distance(d):
-    return np.sqrt(d[0] ** 2 + d[1] ** 2)
-
-
-def find_nearest(array, value):
+def distance_to_array(array, value):
     d = np.abs(array - value)
-    found = tuple(min(d, key=lambda a: distance(a)))
-    for i, a in enumerate(d):
-        if tuple(a) == found:
-            return array[i]
+    distances = np.sqrt(d[:, 0] ** 2 + d[:, 1] ** 2)
+    return distances.min()
 
 
-def voronoi_matrix(size, n_points=30, save=False, plot=False):
+def voronoi_matrix(size, n_points=30, save=True, plot=False):
     points = np.random.randint(0, size, (n_points, 2))
 
     v = Voronoi(points)
@@ -24,9 +18,7 @@ def voronoi_matrix(size, n_points=30, save=False, plot=False):
     m = np.zeros((size, size))
     for x in range(m.shape[0]):
         for y in range(m.shape[1]):
-            nearest = find_nearest(v.vertices, (x, y))
-            d = nearest - (x, y)
-            m[x, y] = -distance(d)
+            m[x, y] = -distance_to_array(v.vertices, (x, y))
 
     if save:
         save_to_img(m.copy())
