5edc2c6a6531878435ad087b24214d923048145f
Bertrand Bordage <bordage.bertrand@gmail.com>
2013-08-12 11:10:34 +0200

    Met à jour la procédure de compilation.

--- README.rst
+++ README.rst
@@ -4,7 +4,7 @@ PyQt OpenGL experiments
 Ubuntu 13.04
 ------------
 
-#. ``sudo apt-get install python-pip python-qt4 python-qt4-gl freeglut3-dev python-numpy cython python-imaging``
-#. ``sudo pip install scipy==0.12.0``
+#. ``sudo apt-get install python-pip python-qt4 python-qt4-gl freeglut3-dev gfortran liblapack-dev``
+#. ``sudo pip install -r requirements.txt``
 #. ``python setup.py build_ext --inplace --force``
 #. ``./main.py``
--- setup.py
+++ setup.py
@@ -1,19 +1,36 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
+
+
+def ext(name, numpy=False, GL=False, GLU=False):
+    kwargs = {
+        'name': name,
+        'sources': [name + '.pyx'],
+        'include_dirs': [],
+        'libraries': [],
+    }
+    if numpy:
+        kwargs['include_dirs'].append(np.get_include())
+    if GL:
+        kwargs['libraries'].append('GL')
+    if GLU:
+        kwargs['libraries'].append('GLU')
+    return Extension(**kwargs)
 
 
 ext_modules = [
-    Extension('utils', ['utils.pyx']),
-    Extension('diamond_square', ['diamond_square.pyx']),
-    Extension('voronoi', ['voronoi.pyx']),
-    Extension('perturbation', ['perturbation.pyx']),
-    Extension('engine', ['engine.pyx'], libraries=['GL', 'GLU']),
+    ext('utils', numpy=True),
+    ext('diamond_square', numpy=True),
+    ext('voronoi', numpy=True),
+    ext('perturbation', numpy=True),
+    ext('engine', GL=True, GLU=True),
 ]
 
 
 setup(
     name='PyQt OpenGL experiments',
     cmdclass={'build_ext': build_ext},
-    ext_modules=ext_modules
+    ext_modules=ext_modules,
 )
